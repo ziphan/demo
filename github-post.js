5381667b2ffbabce878c36969db617b2bd86121c
ziphan <ziphan@qq.com>
2017-07-25 21:16:22 +0800

    edit corner

---
+++ github-post.js
@@ -0,0 +1,13 @@
+!function () {
+  let a = document.createElement('a');
+  let path = location.pathname;
+  let param = {
+    href: 'https://ziphan.github.io',
+    target: '_blank',
+    htmlContent: '<svg width="50" height="50" viewBox="0 0 100 100" style="fill:#32bad8;font-size:40px;position:fixed;top:0;border:0;left:0;"><path d="M0,0 L0,100 L100,0 Z"></path><text x="20" y="45" fill="#fff">Z</text></svg>',
+  };
+  a.setAttribute('href', param.href);
+  a.setAttribute('target', param.target);
+  a.innerHTML = param.htmlContent;
+  document.body.appendChild(a);
+}();
